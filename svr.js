9ae7f906b09f2ffcc17bc2a75689f94467ffe940
Alvie <8447791+AlvieMahmud@users.noreply.github.com>
2020-02-27 19:40:38 +0000

    Return inputs

---
+++ svr.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//
+const express = require('express');
+const app = express();
+app.use(express.static('client', { extensions: ['html'] }));
+app.listen(8080);
+
+let message = "";
+
+app.post('/submit', express.json(), function (req, res) {
+  let message = req.body;
+  console.log(message);
+  res.send(message);
+});
+
+app.get('/submit', (req, res) => {
+  res.send(message);
+});
+
+// app.get('/example', express.json(), function(req, res){
+//   res.send(JSON.stringify(url: ))
+// });
