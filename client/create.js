9ee1c374132b002074823fe6c2f4dac1a25bcbcf
Alvie <8447791+AlvieMahmud@users.noreply.github.com>
2020-05-31 20:50:41 +0100

    Documentation

--- client/create.js
+++ client/create.js
@@ -1,32 +1,40 @@
 'use strict';
 
+// page loaded function
+// adds event listeners
 function pageLoaded () {
 	addListeners();
 }
 
+
+// create question element function
+//     - supply a type 'text' | 'number' | 'single-select' | 'multi-select'
 function createQuestionElem(type) {
+	// create section element, questionElem
 	const questionElem = document.createElement('section');
 	questionElem.classList.add('questionObj');
 
+	// create span element containing the type as its value and text content
 	const typeSpan = document.createElement('span');
 	typeSpan.classList.add('typeSpan');
 	typeSpan.value = type;
 	typeSpan.textContent = type;
 
+	// create p element with an input asking for id
 	const questionIDText = document.createElement('p');
 	questionIDText.textContent = 'Question ID';
-
 	const questionID = document.createElement('input');
 	questionID.type = 'text';
 	questionID.name = 'id';
 
+	// create p element with an input asking for text
 	const questionTextText = document.createElement('p');
 	questionTextText.textContent = 'Question Text';
-
 	const questionText = document.createElement('input');
 	questionText.type = 'text';
 	questionText.name = 'text';
 
+	// append above elements to questionElem
 	questionElem.appendChild(typeSpan);
 	questionElem.appendChild(questionIDText);
 	questionElem.appendChild(questionID);
@@ -34,96 +42,126 @@ function createQuestionElem(type) {
 	questionElem.appendChild(questionText);
 
 	if (type === 'single-select' | type === 'multi-select'){
+		// if type is select, create additional element asking for valid options
 		const validOptionsText = document.createElement('p');
 		validOptionsText.textContent = 'Please enter the valid options (separate by commas)';
 		const validOptions = document.createElement('input');
 		validOptions.type = 'text';
 		validOptions.name = 'options';
-
+		// append above elements to questionElem
 		questionElem.appendChild(validOptionsText);
 		questionElem.appendChild(validOptions);
 	}
 
+	// append questionElem to formCreator
 	const formCreator = document.querySelector('#formCreator');
 	formCreator.appendChild(questionElem);
-
 }
 
+
+// Jsonify function
+// converts page to a valid JSON object
 function Jsonify() {
+	// get formName from input
 	const formName = document.querySelector('#formNameInput').value;
+	// get all question objects
 	const questionObjs = document.querySelectorAll('.questionObj');
 	let idArray = [];
-
+	// create initial formJson object
 	let formJson = {'name': formName, questions: []};
 
+	// for each question, create an object containing id, text, type, and options (if select)
 	for (const questionObj of questionObjs) {
 		let internalJson = {};
+		// for each input element within question object
 		const questionInputs = questionObj.querySelectorAll('input');
 		for (const q of questionInputs){
 			if (q.name === 'id' | q.name === 'text'){
+				// add value and attribute to internal JSON object
 				internalJson[q.name] = q.value;
 				if (q.name === 'id'){
 					if (idArray.includes(q.value)){
+						// if same id is re-used multiple times, return nothing
 						console.log('re-used IDs');
 						console.log(idArray);
 						return;
 					} else {
+						// add id to array (can later be checked for re-use)
 						idArray.push(q.value);
 					}
 				}
 			} else {
+				// if option, then split and trim each option to array
 				internalJson[q.name] = q.value.split(',').map(item => item.trim());
 			}
 			
 		}
+		// get type of question from value of the typeSpan element
 		const typeSpan = questionObj.querySelector('.typeSpan');
 		internalJson['type'] = typeSpan.value;
+		// append internal JSON object of question {id, text, type, (options)}
+		// to formJson.questions
 		formJson['questions'].push(internalJson);
 		
 	}
 	return formJson;
 }
 
+// sign in function (google Auth)
+//     - supply a gUser (handled by Google script in html)
 // eslint-disable-next-line no-unused-vars
-async function onSignIn(gUser) { //when/if user is signed in.	
+async function onSignIn(gUser) { //when/if user is signed in.
+	// allow sign out button to be clickable, and say 'SIGN OUT'
 	const btnSignOut = document.querySelector('#btnSignOut');
 	btnSignOut.disabled = false;
 	btnSignOut.textContent = 'SIGN OUT';
+	// get idToken from authResponse and store it in localStorage
 	const idToken = gUser.getAuthResponse().id_token; // get token
 	localStorage.setItem('idToken', idToken); // and set in local storage
 }
 
-// eslint-disable-next-line no-unused-vars
+// sign out function
 async function onSignOut() {
-	localStorage.setItem('idToken', null);
+	// set idToken in localStorage to '' (no user)
+	localStorage.setItem('idToken', '');
 	const btnSignOut = document.querySelector('#btnSignOut');
+	// disabled sign out button and set to 'SIGNED OUT'
 	btnSignOut.disabled = true;
 	btnSignOut.textContent = 'SIGNED OUT';
+	// handled by Google Script to sign out
 	// eslint-disable-next-line no-undef
 	const auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
 		console.log('User signed out.');
 	});
 }
-
+// create form function
+// called when client clicks upload button
 async function createForm() {
 	const btnCreateForm = document.querySelector('#btnCreateForm');
-	btnCreateForm.disabled = true;
-	btnCreateForm.classList.add('disableBtn');
+	const localToken = localStorage.getItem('idToken');
+	btnCreateForm.disabled = true; // disallow multiple creations
+	// get JSON from html elements
 	const formJson = Jsonify();
 	if (!formJson) {
+		// if formJson is null, log error and don't continue
 		console.log('error');
 		return;
 	}
 
 	let payloadObj = {};
+	// and add formJson to payloadObj.form
 	payloadObj['form'] = formJson;
 
-	if (localStorage.getItem('idToken') !== null) {
-		payloadObj['idToken'] = localStorage.getItem('idToken');
+	if (localToken !== null) {
+		// set idToken to localToken (from localStorage)
+		// may be '' (if signed in then signed out)
+		payloadObj['idToken'] = localToken;
 	} else {
 		payloadObj['idToken'] = '';
 	}
+
+	// POST request to upload form with payloadObj ({idToken, form}) as body
 	const response = await fetch('/upload-form', {
 		method: 'POST',
 		headers: {
@@ -135,47 +173,68 @@ async function createForm() {
 	if (!response.ok) {
 		console.warn('unsuccessful', response);
 	} else {
+		// if successful, response.json() will contain an object
+		// with a .formId and .answersId
 		const data = await response.json();
-
-		const secFormDetails = document.querySelector('#formDetailsHidden');
-		const formIdSpan = document.querySelector('#formIdSpan');
-		const formLinkSpan = document.querySelector('#formLinkSpan');
-		formIdSpan.textContent = data.formId;
-
-		const respIdSpan = document.querySelector('#respIdSpan');
-		const respLinkSpan = document.querySelector('#respLinkSpan');
-		respIdSpan.textContent = data.respId;
-
-		const formIdLink = document.createElement('a');
-		const formURL = `${window.location.origin}/form#${data.formId}`;
-		formIdLink.href = formURL;
-		formIdLink.textContent = formURL;
-		formIdLink.target = '_blank';
-		formLinkSpan.appendChild(formIdLink);
-
-		const respIdLink = document.createElement('a');
-		const respURL = `${window.location.origin}/answers/${data.respId}`;
-		respIdLink.href = respURL;
-		respIdLink.textContent = respURL;
-		respIdLink.target = '_blank';
-		respLinkSpan.appendChild(respIdLink);
-		secFormDetails.id = 'formDetails';
+		// pass data (object) to populateFormDetails
+		populateFormDetails(data);
 	}
 }
 
+// creates and appends a <p> elem with nested <a>
+// with specified href, place, text conent
+// to specified parent
+function createAndAppendLink(href, place, content, parent){
+	const newElem = document.createElement('p');
+	newElem.textContent = `Link to ${place}: `;
+	const link = document.createElement('a');
+	link.textContent = `${content}`;
+	link.href = href;
+	link.target = '_blank'; // open in new tab
+	newElem.appendChild(link); // nest <a> link in <p>
+	parent.appendChild(newElem);
+}
+
+// populate form details function
+//     - supply data (object containing formId & answerId)
+//                    e.g. {formId, answersId}
+function populateFormDetails(data){
+	// select necessary queries
+	const secFormDetails = document.querySelector('#formDetailsHidden');
+	const formIdSpan = document.querySelector('#formIdSpan');
+	const formLinkSpan = document.querySelector('#formLinkSpan');
+	const answersIdSpan = document.querySelector('#answersIdSpan');
+	const answersLinkSpan = document.querySelector('#answersLinkSpan');
+	// populate spans with respective ids (form / answers)
+	formIdSpan.textContent = data.formId;
+	answersIdSpan.textContent = data.answersId;
+	// create links with anchors to form and answers
+	const formURL = `${window.location.origin}/form#${data.formId}`;
+	createAndAppendLink(formURL, 'form', formURL, formLinkSpan);
+	const answersURL = `${window.location.origin}/answers/${data.answersId}`;
+	createAndAppendLink(answersURL, 'answers', answersURL, answersLinkSpan);
+	// set section id to formDetails (removes display: none) so it is visible
+	secFormDetails.id = 'formDetails';
+}
+
+// add listeners function
+// called on page load
 function addListeners(){
 	const formDrop = document.querySelector('#formCreator');
 	const dragObjs = document.querySelectorAll('#formObjects ul li');
-	const btnCreateForm = document.querySelector('#btnCreateForm');
-
+	// prevent default functions of drag over
 	formDrop.addEventListener('dragover', (e) => {
 		e.preventDefault();
 	});
+	// get text on drop, and call createQuestionElemt to build inputs
+	// form question of type from text
 	formDrop.addEventListener('drop', (e) => {
 		let questionType = e.dataTransfer.getData('text/plain');
 		createQuestionElem(questionType);
 	});
 	
+	// for each of the drag objects, when dragstart, set data transfer
+	// of the value of the element (i.e. text | number | single-select | multi-select)
 	dragObjs.forEach(dragObj => {
 		dragObj.addEventListener('dragstart', (e) => {
 
@@ -183,9 +242,14 @@ function addListeners(){
 		});
 	});
 
+	// when btnCreateForm is clicked, upload the JSON to server (createForm function)	
+	const btnCreateForm = document.querySelector('#btnCreateForm');
 	btnCreateForm.addEventListener('click', createForm);
+
+	// when btnSignOut is clicked, call onSignOut function
+	const btnSignOut = document.querySelector('#btnSignOut');
+	btnSignOut.addEventListener('click', onSignOut);
 }
 
-
-
+// add listener for when page loads and call pageLoaded function
 window.addEventListener('load', pageLoaded);
--- client/index.js
+++ client/index.js
@@ -1,32 +1,48 @@
 'use strict';
 
+
+// page loaded function
+// adds event listeners
 function pageLoaded () {
 	addListeners();
 }
 
+// creates and appends a <p> elem with specified
+// text content to specified parent
 function createAndAppend(content, parent){
 	const newElem = document.createElement('p');
 	newElem.textContent = content;
 	parent.appendChild(newElem);
 }
 
-function createAndAppendLink(href, place, parent){
+// creates and appends a <p> elem with nested <a>
+// with specified href, place, text conent
+// to specified parent
+function createAndAppendLink(href, place, content, parent){
 	const newElem = document.createElement('p');
 	newElem.textContent = `Link to ${place}: `;
 	const link = document.createElement('a');
-	link.textContent = `${place}`;
+	link.textContent = `${content}`;
 	link.href = href;
-	newElem.appendChild(link);
+	link.target = '_blank'; // open in new tab
+	newElem.appendChild(link); // nest <a> link in <p>
 	parent.appendChild(newElem);
 }
 
-async function populateAnswerZone() {
+// populates answer zone
+// called upon sign in
+// adds details of Forms
+async function populateFormsZone() {
 	//const formArray = some http request.
 	const idObj = {
 		'idToken': localStorage.getItem('idToken')
 	};
-	const viewAnswerZone = document.querySelector('#viewAnswerZone');
-	viewAnswerZone.textContent = '';
+	const viewForms = document.querySelector('#viewForms');
+	// remove existing content from viewForms section
+	viewForms.textContent = '';
+
+	// make POST request to get details of all forms
+	// related to logged in user
 	const response = await fetch('/all-user-forms', {
 		method: 'POST',
 		headers: {
@@ -35,41 +51,59 @@ async function populateAnswerZone() {
 		body: JSON.stringify(idObj)
 	});
 	const formArray = await response.json();
-	console.log(formArray);
-	for (const formDetailObj of formArray.userFormObjs) {
-		const formItem = document.createElement('section');
-		formItem.classList.add('formItem');
-		createAndAppend(formDetailObj.formName, formItem);
-		createAndAppendLink(`/form#${formDetailObj.formId}`, 'Form', formItem);
-		createAndAppendLink(`/answers/${formDetailObj.answersId}`, 'Answers', formItem);
-		createAndAppend(`Form ID: ${formDetailObj.formId}`, formItem);
-		createAndAppend(`Answers ID: ${formDetailObj.answersId}`, formItem);
-		viewAnswerZone.appendChild(formItem);
-	}
 
-	
+	// for each form of the array of forms, add it to viewForms
+	for (const formDetailObj of formArray.userFormObjs) {
+		createFormItemSection(formDetailObj);
+	}
 }
 
+// create form item section function
+//    - supply a formDetailObj e.g. {formName, formId, answersId}
+//    appends to viewForms
+function createFormItemSection(formDetailObj) {
+	const viewForms = document.querySelector('#viewForms');
+	// create section of class formItem
+	const formItem = document.createElement('section');
+	formItem.classList.add('formItem');
+	// append details such as names and links to formItem section
+	createAndAppend(formDetailObj.formName, formItem);
+	createAndAppendLink(`/form#${formDetailObj.formId}`, 'Form', 'Form', formItem);
+	createAndAppendLink(`/answers/${formDetailObj.answersId}`, 'Answers', 'Answers', formItem);
+	createAndAppend(`Form ID: ${formDetailObj.formId}`, formItem);
+	createAndAppend(`Answers ID: ${formDetailObj.answersId}`, formItem);
+	// append formItem section as a child of viewForms
+	viewForms.appendChild(formItem);
+}
 
+// sign in function (google Auth)
+//     - supply a gUser (handled by Google script in html)
 // eslint-disable-next-line no-unused-vars
 async function onSignIn(gUser) { //when/if user is signed in.	
+	// allow sign out button to be clickable, and say 'SIGN OUT'
 	const btnSignOut = document.querySelector('#btnSignOut');
 	btnSignOut.disabled = false;
 	btnSignOut.textContent = 'SIGN OUT';
-	const idToken = gUser.getAuthResponse().id_token; // get token
-	localStorage.setItem('idToken', idToken); // and set in local storage
-	populateAnswerZone();
+	// get idToken from authResponse and store it in localStorage
+	const idToken = gUser.getAuthResponse().id_token;
+	localStorage.setItem('idToken', idToken);
+	// users may have forms already, populate forms zone with their forms
+	populateFormsZone();
 }
 
-// eslint-disable-next-line no-unused-vars
+// sign out function
 async function onSignOut() {
-	localStorage.setItem('idToken', 'null');
+	// set idToken in localStorage to '' (no user)
+	localStorage.setItem('idToken', '');
 	const btnSignOut = document.querySelector('#btnSignOut');
-	const viewAnswerZone = document.querySelector('#viewAnswerZone');
-	viewAnswerZone.textContent = '';
-	createAndAppend('You must be signed in to view this section', viewAnswerZone);
+	const viewForms = document.querySelector('#viewForms');
+	// remove any existing form information from viewForms and display sign in msg
+	viewForms.textContent = '';
+	createAndAppend('You must be signed in to view this section', viewForms);
+	// disabled sign out button and set to 'SIGNED OUT'
 	btnSignOut.disabled = true;
 	btnSignOut.textContent = 'SIGNED OUT';
+	// handled by Google Script to sign out
 	// eslint-disable-next-line no-undef
 	const auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
@@ -77,29 +111,31 @@ async function onSignOut() {
 	});
 }
 
+// function to make decision on upload
 function decideUpload() {
 	const inputFile = document.querySelector('#newFile');
 	const btnUpload = document.querySelector('#btnUpload');
 
-	if(inputFile.files.length == 0 ){ 
+	if(inputFile.files.length == 0 ){ // if no file submitted
 		console.log('empty');
 	} else {
 		// get file extension
 		if (inputFile.files[0].name.split('.')[1].toLowerCase() !== 'json') {
+			// if file is not .json / .JSON etc
 			console.log('wrong file type');
-			// add disable button class if it not already existing
-			if (!btnUpload.classList.contains('disableBtn')){
-				btnUpload.disabled = true;
-				btnUpload.classList.add('disableBtn');
-			}
+			btnUpload.disabled = true;
 		} else {
+			// allow upload button to be clicked
 			btnUpload.disabled = false;
-			btnUpload.classList.remove('disableBtn');
 		}
 	}
 }
 
+
+// function to get JSON contents of a file
+//     - supply an inputFile
 async function getJsonContents(inputFile) {
+	// instatiate a FileReader
 	const reader = new FileReader();
 	return new Promise((resolve, reject) => {
 		reader.onerror = () => {
@@ -107,27 +143,33 @@ async function getJsonContents(inputFile) {
 			reject(new DOMException('Problem parsing input file.'));
 		};
 		reader.onload = () => {
+			// resolve with contents of reader (.result)
 			resolve(reader.result);
 		};
 		reader.readAsText(inputFile);
 	});
 }
 
+// upload json (form) function
+// called when client clicks upload button
 async function uploadJson() {
 	const btnUpload = document.querySelector('#btnUpload');
 	const inputFile = document.querySelector('#newFile');
-
-	btnUpload.classList.add('disableBtn');
-	btnUpload.disabled = true;
+	const localToken = localStorage.getItem('idToken');
+	btnUpload.disabled = true; // disallow multiple uploads
 
 	let payloadObj = {};
+	// get contents of JSON file and add to payloadObj.form
 	payloadObj['form'] = JSON.parse(await getJsonContents(inputFile.files[0]));
-	if (localStorage.getItem('idToken') !== null) {
-		payloadObj['idToken'] = localStorage.getItem('idToken');
+	if (localToken !== null) {
+		// set idToken to localToken (from localStorage)
+		// may be '' (if signed in then signed out)
+		payloadObj['idToken'] = localToken;
 	} else {
 		payloadObj['idToken'] = '';
 	}
 
+	// POST request to upload form with payloadObj ({idToken, form}) as body
 	const response = await fetch('/upload-form', {
 		method: 'POST',
 		headers: {
@@ -139,42 +181,51 @@ async function uploadJson() {
 	if (!response.ok) {
 		console.warn('unsuccessful', response);
 	} else {
+		// if successful, response.json() will contain an object
+		// with a .formId and .answersId
 		const data = await response.json();
-
-		const secFormDetails = document.querySelector('#formDetailsHidden');
-		const formIdSpan = document.querySelector('#formIdSpan');
-		const formLinkSpan = document.querySelector('#formLinkSpan');
-		formIdSpan.textContent = data.formId;
-
-		const answersIdSpan = document.querySelector('#answersIdSpan');
-		const answersLinkSpan = document.querySelector('#answersLinkSpan');
-		answersIdSpan.textContent = data.answersId;
-
-		const formIdLink = document.createElement('a');
-		const formURL = `${window.location.origin}/form#${data.formId}`;
-		formIdLink.href = formURL;
-		formIdLink.textContent = formURL;
-		formIdLink.target = '_blank';
-		formLinkSpan.appendChild(formIdLink);
-
-		const answersIdLink = document.createElement('a');
-		const respURL = `${window.location.origin}/answers/${data.answersId}`;
-		answersIdLink.href = respURL;
-		answersIdLink.textContent = respURL;
-		answersIdLink.target = '_blank';
-		answersLinkSpan.appendChild(answersIdLink);
-		secFormDetails.id = 'formDetails';
+		// pass data (object) to populateFormDetails
+		populateFormDetails(data);
 	}
-
 }
 
+// populate form details function
+//     - supply data (object containing formId & answerId)
+//                    e.g. {formId, answersId}
+function populateFormDetails(data){
+	// select necessary queries
+	const secFormDetails = document.querySelector('#formDetailsHidden');
+	const formIdSpan = document.querySelector('#formIdSpan');
+	const formLinkSpan = document.querySelector('#formLinkSpan');
+	const answersIdSpan = document.querySelector('#answersIdSpan');
+	const answersLinkSpan = document.querySelector('#answersLinkSpan');
+	// populate spans with respective ids (form / answers)
+	formIdSpan.textContent = data.formId;
+	answersIdSpan.textContent = data.answersId;
+	// create links with anchors to form and answers
+	const formURL = `${window.location.origin}/form#${data.formId}`;
+	createAndAppendLink(formURL, 'form', formURL, formLinkSpan);
+	const answersURL = `${window.location.origin}/answers/${data.answersId}`;
+	createAndAppendLink(answersURL, 'answers', answersURL, answersLinkSpan);
+	// set section id to formDetails (removes display: none) so it is visible
+	secFormDetails.id = 'formDetails';
+}
+
+// add listeners function
+// called on page load
 function addListeners() {
+	// when inputFile changes, decide whether upload is allowed (decideUpload function)
 	const inputFile = document.querySelector('#newFile');
 	inputFile.addEventListener('change', decideUpload);
+
+	// when btnUpload is clicked, upload the JSON to server (uploadJson function)
 	const btnUpload = document.querySelector('#btnUpload');
 	btnUpload.addEventListener('click', uploadJson);
+
+	// when btnSignOut is clicked, call onSignOut function
 	const btnSignOut = document.querySelector('#btnSignOut');
 	btnSignOut.addEventListener('click', onSignOut);
 }
 
+// add listener for when page loads and call pageLoaded function
 window.addEventListener('load', pageLoaded);
